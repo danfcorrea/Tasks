495bafc76f1416e612efb38753675d8a9897eed4
Daniel Corrêa <daniel.1997.df@gmail.com>
2024-06-07 11:21:55 -0300

    feat: implementation of task register

--- app/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
@@ -2,31 +2,28 @@ package com.devmasterteam.tasks.service.repository
 
 import android.content.Context
 import com.devmasterteam.tasks.R
-import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PriorityModel
 import com.devmasterteam.tasks.service.repository.local.TaskDatabase
 import com.devmasterteam.tasks.service.repository.remote.PriorityService
 import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
-import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PriorityRepository(val context: Context) {
+class PriorityRepository(val context: Context) : BaseRepository() {
 
     private val remote = RetrofitClient.getService(PriorityService::class.java)
     private val database = TaskDatabase.getDatabase(context).priorityDAO()
 
-    fun list(listener: APIListener<List<PriorityModel>>){
+    fun list(listener: APIListener<List<PriorityModel>>) {
         val call = remote.list()
         call.enqueue(object : Callback<List<PriorityModel>> {
-            override fun onResponse(call: Call<List<PriorityModel>>, response: Response<List<PriorityModel>>) {
-                if(response.code() == TaskConstants.HTTP.SUCCESS){
-                    response.body()?.let { listener.onSuccess(it) }
-                }else{
-                    response.errorBody().let { if (it != null) listener.onFailure(failResponse(it.string())) }
-                }
+            override fun onResponse(
+                call: Call<List<PriorityModel>>,
+                response: Response<List<PriorityModel>>
+            ) {
+                handleResponse(response, listener)
             }
 
             override fun onFailure(call: Call<List<PriorityModel>>, t: Throwable) {
@@ -36,11 +33,12 @@ class PriorityRepository(val context: Context) {
 
     }
 
-    fun save(list: List<PriorityModel>){
+    fun list(): List<PriorityModel> {
+        return database.list()
+    }
+
+    fun save(list: List<PriorityModel>) {
         database.clear()
         database.save(list)
-    }
-    private fun failResponse(str: String):String{
-        return Gson().fromJson(str, String::class.java)
     }
 }
